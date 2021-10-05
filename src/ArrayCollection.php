f400f33852a566512161ed2f908d6007af350d54
Mamadou Aly Sy <sy.aly.mamadou@gmail.com>
2021-10-05 23:09:40 +0000

    first commit

---
+++ src/ArrayCollection.php
@@ -0,0 +1,57 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace MamadouAlySy;
+
+class ArrayCollection
+{
+    protected array $items;
+
+    public function __construct(array $items = [])
+    {
+        $this->items = $items;
+    }
+
+    public function all(): array
+    {
+        return $this->items;
+    }
+
+    public function add(string $key, mixed $value): void
+    {
+        $this->items[$key] = $value;
+    }
+
+    public function contains(string $key): bool
+    {
+        return array_key_exists($key, $this->items);
+    }
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return $this->contains($key) ? $this->items[$key] : $default;
+    }
+
+    public function remove(string $key): void
+    {
+        unset($this->items[$key]);
+    }
+
+    public function clone (): self {
+        return clone $this;
+    }
+
+    public function flush(string $key): mixed
+    {
+        $value = $this->get($key);
+        $this->remove($key);
+
+        return $value;
+    }
+
+    public function getCount(): int
+    {
+        return count($this->items);
+    }
+}
---
+++ tests/ArrayCollectionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use MamadouAlySy\ArrayCollection;
+
+class ArrayCollectionTest extends TestCase
+{
+    protected ArrayCollection $bag;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->bag = new ArrayCollection(['item1' => 1]);
+    }
+
+    public function testCanStoreAValueInsideTheBag()
+    {
+        $this->bag->add('item2', 2);
+        $this->assertEquals(2, $this->bag->getCount());
+    }
+
+    public function testCanChecksIfAValueExistInsideTheBag()
+    {
+        $this->assertTrue($this->bag->contains('item1'));
+    }
+
+    public function testCanGetAValueInsideTheBag()
+    {
+        $this->assertEquals(1, $this->bag->get('item1'));
+    }
+
+    public function testCanRemoveAValueInsideTheBag()
+    {
+        $this->bag->remove('item1');
+        $this->assertFalse($this->bag->contains('item1'));
+    }
+}
